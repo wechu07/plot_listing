b0a325d66bebd88cd1b71711710359dba549af04
wechu07 <wechuli017@gmail.com>
2022-09-06 10:28:05 +0300

    seeds commit

---
+++ seeds/lists.js
@@ -0,0 +1,12 @@
+module.exports = [
+    {
+        title: "Best one in this category",
+        by: "Real ESTATE Agent",
+        price: "$800 / month included tax",
+        details: "860 sq ft",
+        info: {
+            image: 
+        },
+        
+    }
+]
