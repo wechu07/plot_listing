8e65224a78f1d7c4abf429d824b68c130b3711a0
wechu07 <wechuli017@gmail.com>
2022-09-11 22:14:33 +0300

    buggy nightmare for today

--- app.js
+++ app.js
@@ -1,62 +1,53 @@
-if (process.env.NODE_ENV !== "production") {
-    require('dotenv').config();
-} //if we are still in production, pick the data from .env file
+const dotenv = require('dotenv')
+const express = require('express')
+const path = require('path')
+const connectDB = require('./config/db')
+const morgan = require('morgan')
+const { engine } = require('express-handlebars')
+const methodOverride = require('method-override')
+const Listing = require('./models/listings')
 
-// const { config } = require('dotenv');
-const dotenv = require('dotenv');
-const express = require('express');
-const path = require('path');
-const ejsMate = require('ejs-mate');
-const methodOverride = require('method-override');
-const mongoose = require('mongoose');
-const dbUrl = process.env.DB_URL;
-const Listing = require('./models/listings');
+// loading the config files
+dotenv.config({ path: './config/config.env' })
 
-mongoose.connect(dbUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-});
+// connect the db
+connectDB()
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", () => {
-    console.log("Database connected");
-});
+const app = express()
 
-const app = express();
+// Body parser
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
 
-const port = process.env.PORT || 9000;
+// HTTP request logger middleware 
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
 
-app.engine('ejs', ejsMate);
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+// Handlebars
+app.engine('.hbs',engine({defaultLayout: 'main',extname: '.hbs'}))
+app.set('view engine', '.hbs')
+app.set('views',path.join(__dirname,'./views'))
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+// routes
+app.use('/', require('./routes/index'))
+
+const port = process.env.PORT || 3000;
+
+// app.use(methodOverride('_method'));
+
+// loading static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.render('home');
-});
+// app.use((err, req, res, next) => {
+//     const { statusCode = 500 } = err;
+//     if (!err.message) err.message = 'Oh No, Something Went Wrong!'
+//     res.status(statusCode).render('error', { err })
+// })
 
-app.get('/contact-us', (req, res) => {
-    res.render('contact-us');
-});
 
-app.get('/category', (req, res) => {
-    res.render('category');
-});
-
-app.get('/listing', (req, res) => {
-    res.render('listings/listing');
-});
-
-app.use((err, req, res, next) => {
-    const { statusCode = 500 } = err;
-    if (!err.message) err.message = 'Oh No, Something Went Wrong!'
-    res.status(statusCode).render('error', { err })
-})
-
+// server running in either
+// production or development mode
 app.listen(port, () => {
-    console.log(`Server running on: http://localhost:${port}`);
+    console.log(`Server running in ${process.env.NODE_ENV} mode on http://localhost:${port}`);
 });
