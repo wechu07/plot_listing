b254e4665f9fb5c35be9a9a9d60b72e1179b2f2a
wechu07 <wechuli017@gmail.com>
2022-09-11 17:40:14 +0300

    update db connection code

--- config/db.js
+++ config/db.js
@@ -2,11 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-      const db = await mongoose.connect(process.env.DB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-  
+      const db = await mongoose.connect(process.env.DB_URL)
       console.log(`Database connected: ${db.connection.host}`)
     } catch (err) {
       console.error(err)
