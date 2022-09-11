7ca0e265bd38fc22cbf331c4bc2de7555aed261c
wechu07 <wechuli017@gmail.com>
2022-09-11 16:51:47 +0300

    db connection success

---
+++ config/db.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
+    try {
+      const db = await mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
+  
+      console.log(`Database connected: ${db.connection.host}`)
+    } catch (err) {
+      console.error(err)
+      process.exit(1)
+    }
+  }
+
+  module.exports = connectDB
+
+// alternative code
+
+// mongoose.connect(dbUrl, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+// });
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", () => {
+//     console.log("Database connected");
+// });
