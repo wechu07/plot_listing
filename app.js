3644392b344ee2734f26628bef0c18976603f51e
wechu07 <wechuli017@gmail.com>
2022-09-26 19:53:57 +0300

    add new routes

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ const connectDB = require('./config/db')
 const morgan = require('morgan')
 const { engine } = require('express-handlebars')
 const methodOverride = require('method-override')
+const passport = require('passport')
+const session = require('express-session')
 const Listing = require('./models/Listing')
+const makeMiddleware = require('multer/lib/make-middleware')
 
 // loading the config files
 dotenv.config({ path: './config/config.env' })
+
+// passport config
+require('./config/passport')(passport)
 
 // connect the db
 connectDB()
@@ -27,11 +33,23 @@ if (process.env.NODE_ENV === 'development') {
 // Handlebars
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
-app.set("views", path.resolve(__dirname, "./views"));
+app.set("views", path.resolve(__dirname, "./views"))
+
+// express session middleware
+app.use(session({
+    secret: 'thisshouldbeasecret',
+    resave: false, // do not save session if nothing is changed
+    saveUninitialized: false // do not create a session until something is stored
+}))
+
+// passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
 
 // routes
 app.use('/', require('./routes/index'))
-app.use('/', require('./routes/listing'))
+app.use('/auth', require('./routes/auth'))
+app.use('/listings', require('./routes/listing'))
 
 const port = process.env.PORT || 3000;
 
