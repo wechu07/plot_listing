8da2bf49d297b555b20aa487af4c000ce9ffed2c
wechu07 <wechuli017@gmail.com>
2022-09-28 15:48:20 +0300

    add logout user

--- routes/auth.js
+++ routes/auth.js
@@ -17,5 +17,12 @@ router.get('/google/callback', passport.authenticate('google', { failureRedirect
     res.redirect('/')
 })
 
+// @desc Logout user
+// @route  /auth/logout
+router.get('/logout', (req, res) => {
+    req.logout()
+    res.redirect('/')
+})
+
 
 module.exports = router
